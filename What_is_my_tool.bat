40a62f35f567162d3d8752ca0d3fa8e15e12b9e7
Regifyzer <downlowish@yahoo.com>
2024-07-26 09:50:05 -0400

    What_is_my_tool v1.0.1
    v1.0.1

--- What_is_my_tool.bat
+++ What_is_my_tool.bat
@@ -1,12 +1,12 @@
 @echo off
-Title What Is My V1.0
+Title What Is My v1.0
 color 0A
 :start
 cls
 echo.
 echo.
 echo					#####################################
-echo						What Is My? Tool
+echo						What Is My? Tool v1.0.1
 echo.
 echo						By Regifyzer
 echo					#####################################
@@ -23,14 +23,26 @@ echo.
 set /P A=Answer:
 if %A%==1 goto 1
 if %A%==2 goto 2
-if %A%==2 goto 3
-if %A%==2 goto 4
+if %A%==3 goto 3
+if %A%==4 goto 4
 
 :1
 cls
-ipconfig
+setlocal
+setlocal enabledelayedexpansion
+for /f "usebackq tokens=2 delims=:" %%a in (`ipconfig ^| findstr /r "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"`) do (
+  set _temp=%%a
+  set _ipAddress=!_temp:~1!
+  echo Your IP Address is: !_ipAddress!
+echo.
 echo.
 echo Press r to return to the menu or press any other key to exit
+goto :choice
+  )
+endlocal
+echo.
+
+:Choice
 set /p C=Answer:
 if %c%==r (goto start
 ) else (
@@ -67,7 +79,8 @@ pause>null
 
 :4
 cls
-ipconfig && hostname && getmac
+start /b ipconfig
+start /b hostname
 echo.
 echo Press r to return to the menu or press any other key to exit
 set /p C=Answer:
